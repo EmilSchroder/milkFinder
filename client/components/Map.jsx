3f2d0ace969baebab1c1935207bb53f604be55e0
EmilSchroder <eschro66@gmail.com>
2018-09-08 08:59:00 +1200

    A marker has been acheived on the map

--- client/components/Map.jsx
+++ client/components/Map.jsx
@@ -1,12 +1,33 @@
 import React from 'react'
+import GoogleMapReact from 'google-map-react'
 
-const Map = () => {
-  return (
-    <React.Fragment>
-        <h1>This is a map</h1>  
-    </React.Fragment>
+import Marker from './Marker'
 
-  )
+class Map extends React.Component {
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return(
+            <div style={{ height: '100vh', width: '100%' }}>
+            <GoogleMapReact
+              bootstrapURLKeys={{key:"AIzaSyC1GuaSVOn1QvNgS0ysm9mH4V7c7yurAoI"}}
+              defaultCenter={ {lat: -41.2969757, lng: 174.7742823} }
+              defaultZoom={19}
+            >
+            <Marker 
+                lat={-41.2969757}
+                lng={174.7742823}
+                />
+              {/* <AnyReactComponent
+                lat={59.955413}
+                lng={30.337844}
+                text={'Kreyser Avrora'}
+              /> */}
+            </GoogleMapReact>
+          </div>
+        )
+    }
 }
 
 export default Map
---
+++ client/components/Marker.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import GoogleMapReact from 'google-map-react'
+
+class Marker extends React.Component {
+    constructor(props){
+        super(props)
+    console.log(props)
+    }
+    
+    render(){
+        return(
+
+  <div style={{
+    color: 'white', 
+    background: 'grey',
+    padding: '15px 10px',
+    display: 'inline-flex',
+    textAlign: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: '100%',
+    transform: 'translate(-50%, -50%)'
+  }}>
+    Emil
+  </div>
+        )
+    }
+}
+
+export default Marker
