eb579aced2398e120521cd7198034842b80de4ca
EmilSchroder <eschro66@gmail.com>
2018-09-08 09:20:10 +1200

    db set up

---
+++ migrations/20180908091440_Cafes.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('Cafes', table =>{
+    table.increments('id').primary
+    table.string('cafe')
+    table.boolean('cow')
+    table.boolean('soy')
+    table.boolean('almond')
+    table.boolean('coconut')
+    table.boolean('rice')
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('Cafes')
+};
